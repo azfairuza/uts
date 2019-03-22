166624bec0bf649c22ec034471eadd9a895c1e9d
azfairuza <fairuza.zacky1@gmail.com>
2019-03-22 22:02:47 +0700

    create hello.js code

--- soal4/hello.js
+++ soal4/hello.js
@@ -1,1 +1,28 @@
-this is hello javascript 
+var ta;
+
+main();
+
+function main()
+{
+    creataAndArrangeElement(ta);
+    loadTextTo(ta);
+}
+
+function creataAndArrangeElement()
+{
+    var arg0 = document.createElement("textarea");
+    arg0.id = arguments[0];
+    arg0.style.width = "400px";
+    arg0.style.height = "100px";
+    arg0.style.float = "center";
+
+    document.body.append(arg0);
+}
+
+function loadTextTo()
+{
+    var id = arguments[0];
+    var contain = document.getElementById(id);
+    contain.value = "Hello, Achmad Zacky Fairuza"
+    + " yang ber-NIM 10215005!";
+}
